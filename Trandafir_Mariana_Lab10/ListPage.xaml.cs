a66a2c1177a92b4cafed87208c40091b56b4e579
Mariana Trandafir <trandafirmariana08@outlook.com>
2022-01-12 23:48:46 +0200

    lab11

--- Trandafir_Mariana_Lab10/ListPage.xaml.cs
+++ Trandafir_Mariana_Lab10/ListPage.xaml.cs
@@ -29,5 +29,21 @@ namespace Trandafir_Mariana_Lab10
         {
             InitializeComponent();
         }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
